577cbcd4e7e27daeef7998746e3a7214310418f3
shotaroikeda <ikeda.shot@gmail.com>
2015-01-08 23:18:19 -0800

    first commit

--- globalvalues.py
+++ globalvalues.py
@@ -1,7 +1,19 @@
 import syschar
 
 global p1
+# p1 is an instance of syschar.PlayerCharacter.
+# p1 will keep track of the player's statuses
+
 global ai
+# ai is an instance of syschar.EnemyCharacter or syschar.BossCharacter.
+# Keeps track of enemy things DURING combat
+
+global runcounter
+# runcounter keeps track of how many times the player has run away from battle
+# only counts consecutive runs
+# hopefully will prevent players from running away from battle all the time
+# resets only when the enemy is defeated. (check syscombat.Combat.enemydefeated)
 
 global cont_mssg
 cont_mssg = "Press Enter to Continue..."
+# Just a shortcut because I'm too lazy to type the message over and over
